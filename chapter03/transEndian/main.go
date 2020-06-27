0e226bce1d74f009769e9243ae83d9d3d2459f64
2222-42 <stephen0617@me.com>
2020-06-28 08:35:34 +0900

    chore(chapter3): finish 3.5.2

---
+++ chapter03/transEndian/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func main() {
+	// 32 ビットのビッグエンディアンのデータ（10000）
+	data := []byte{0x0, 0x0, 0x27, 0x10}
+	var i int32
+
+	// エンディアンの変換
+	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
+	//fmt.Printf("data: %d\n")
+	fmt.Printf("data: %d\n", i)
+}
