6f3e9319d32227dba88b3e23be68703299ccd721
2222-42 <stephen0617@me.com>
2020-07-06 11:31:04 +0900

    chore(chapter3): finish Q3.5

---
+++ chapter03/Q3_5_makeMyCopyN/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func myCopyN(w io.Writer, r io.Reader, n int64) (int64, error) {
+	reader := io.LimitReader(r, n)
+	written, err := io.Copy(w, reader)
+	if written == n {
+		return n, nil
+	}
+	if written < n && err == nil {
+		err = io.EOF
+	}
+	return written, err
+}
+
+func main() {
+	r := strings.NewReader("123456789012345")
+	result1, err1 := myCopyN(os.Stdout, r, 10)
+	fmt.Printf("result: %d, err: %v\n", result1, err1)
+	result2, err2 := myCopyN(os.Stdout, r, 100)
+	fmt.Printf("result: %d, err: %v\n", result2, err2)
+}
