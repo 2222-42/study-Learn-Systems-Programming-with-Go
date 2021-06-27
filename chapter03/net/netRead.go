be2c4cf49f3f3737dc7c14f0a6a03b03f3e5583b
2222-42 <stephen0617@me.com>
2021-06-27 15:32:04 +0900

    chore(*): add memo up to p.48

--- chapter03/net/netRead.go
+++ chapter03/net/netRead.go
@@ -9,16 +9,25 @@ import (
 	"os"
 )
 
+// 3.4.3
 func main() {
-	conn, err := net.Dial("tcp", "ascii.jp:80")
+	conn, err := net.Dial("tcp", "www.google.com:80")
 	if err != nil {
 		panic(err)
 	}
 
-	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	if _, err := conn.Write([]byte("GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n")); err != nil {
+		panic(err)
+	}
+	// net.Conn型のconn(通信内容そのもの)をそのままコピーするのは効率的ではない。
 	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println(res.Header)
 	defer res.Body.Close()
-	io.Copy(os.Stdout, res.Body)
+	if _, err := io.Copy(os.Stdout, res.Body); err != nil {
+		panic(err)
+	}
 }
