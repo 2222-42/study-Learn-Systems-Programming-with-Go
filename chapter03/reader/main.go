5038920f5bf49059eb3798fc5dd64117d94683cd
2222-42 <stephen0617@me.com>
2021-06-27 12:57:04 +0900

    fix(chap3/reader): defer close

--- chapter03/reader/main.go
+++ chapter03/reader/main.go
@@ -13,6 +13,7 @@ func main() {
 		fmt.Printf("os open, err: %v", err)
 		return
 	}
+	defer file.Close()
 	buffer, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Printf("read all, err: %v", err)
@@ -25,6 +26,7 @@ func main() {
 		fmt.Printf("os open, err: %v", err)
 		return
 	}
+	defer file2.Close()
 	buffer2 := make([]byte, 4)
 	size, err := io.ReadFull(file2, buffer2)
 	if err != nil {
