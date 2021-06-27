33d0e6f26b1278ea920165545bec1368a8165c27
2222-42 <stephen0617@me.com>
2021-06-27 12:46:33 +0900

    feat(chap3): use helper function for io.Read

---
+++ chapter03/reader/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("main.go")
+	if err != nil {
+		fmt.Printf("os open, err: %v", err)
+		return
+	}
+	buffer, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("read all, err: %v", err)
+		return
+	}
+	fmt.Printf("read all: %v", string(buffer))
+
+	file2, err := os.Open("main.go")
+	if err != nil {
+		fmt.Printf("os open, err: %v", err)
+		return
+	}
+	buffer2 := make([]byte, 4)
+	size, err := io.ReadFull(file2, buffer2)
+	if err != nil {
+		fmt.Printf("read full, err: %v", err)
+		return
+	}
+	fmt.Printf("read full: %v, size: %v", string(buffer2), size)
+
+}
