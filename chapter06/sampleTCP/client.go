fbb00fdd2fa9046da23506ce1991662c807c782b
2222-42 <stephen0617@me.com>
2021-07-17 08:40:05 +0900

    feat(chap6): client using tcp socket

--- chapter06/sampleTCP/client.go
+++ chapter06/sampleTCP/client.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
+	"net/http/httputil"
 )
 
 // 6.4
+// 6.5.2
 func main() {
-	conn, err := net.Dial("tcp", ":8080")
+	conn, err := net.Dial("tcp", ":8888")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("connected: %v\n", conn.RemoteAddr())
+	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := request.Write(conn); err != nil {
+		panic(err)
+	}
+	response, err := http.ReadResponse(bufio.NewReader(conn), request)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(dump))
 }
