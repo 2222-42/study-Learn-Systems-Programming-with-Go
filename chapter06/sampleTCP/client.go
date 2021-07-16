54ce2b9e8d8461c34a8465cea9a9115798495970
2222-42 <stephen0617@me.com>
2021-07-17 08:59:07 +0900

    feat(chap6): client supporting keep-alive

--- chapter06/sampleTCP/client.go
+++ chapter06/sampleTCP/client.go
@@ -6,33 +6,59 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 // 6.4
 // 6.5.2
+// 6.6.2
 func main() {
-	conn, err := net.Dial("tcp", ":8888")
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
+	current := 0
+	var conn net.Conn = nil
 
-	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
-	if err != nil {
-		panic(err)
-	}
+	for {
+		var err error
 
-	if err := request.Write(conn); err != nil {
-		panic(err)
-	}
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
+		if conn == nil {
+			conn, err = net.Dial("tcp", ":8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
 
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
+		request, err := http.NewRequest("GET", "http://localhost:8888", strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Println(string(dump))
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		response, err := http.ReadResponse(bufio.NewReader(conn), request)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(string(dump))
+		current++
+		if current == len(sendMessages) {
+			break
+		}
+	}
+	conn.Close()
 }
