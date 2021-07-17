5c3397c5f96e69523f1d814887820cd6db269009
2222-42 <stephen0617@me.com>
2021-07-18 07:24:47 +0900

    feat(chap6): server supporting gzip

--- chapter06/sampleTCP/server/server.go
+++ chapter06/sampleTCP/server/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,6 +14,80 @@ import (
 	"strings"
 	"time"
 )
+
+// 6.7.2
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ", "), "gzip") != -1
+}
+
+func processSession(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+
+	for {
+		if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+			panic(err)
+		}
+
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		response := http.Response{
+			Status:           "",
+			StatusCode:       200,
+			Proto:            "",
+			ProtoMajor:       1,
+			ProtoMinor:       1,
+			Header:           make(http.Header),
+			Body:             nil,
+			TransferEncoding: nil,
+			Close:            false,
+			Uncompressed:     false,
+			Trailer:          nil,
+			Request:          nil,
+			TLS:              nil,
+		}
+
+		if isGZipAcceptable(request) {
+			content := "Hello World (gzipped)\n"
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			if _, err := io.WriteString(writer, content); err != nil {
+				panic(err)
+			}
+
+			if err := writer.Close(); err != nil {
+				panic(err)
+			}
+
+			response.Body = ioutil.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = ioutil.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		if err := response.Write(conn); err != nil {
+			panic(err)
+		}
+	}
+}
 
 // 6.5
 // 6.6.1
@@ -29,55 +105,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-
-			for {
-				if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
-					panic(err)
-				}
-
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					neterr, ok := err.(net.Error)
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
-				content := "Hello World\n"
-
-				response := http.Response{
-					Status:           "",
-					StatusCode:       200,
-					Proto:            "",
-					ProtoMajor:       1,
-					ProtoMinor:       1,
-					Header:           nil,
-					Body:             ioutil.NopCloser(strings.NewReader(content)),
-					ContentLength:    int64(len(content)),
-					TransferEncoding: nil,
-					Close:            false,
-					Uncompressed:     false,
-					Trailer:          nil,
-					Request:          nil,
-					TLS:              nil,
-				}
-				if err := response.Write(conn); err != nil {
-					panic(err)
-				}
-			}
-		}()
+		go processSession(conn)
 
 	}
 }
