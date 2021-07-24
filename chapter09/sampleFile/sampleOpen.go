26197a9c71ba88191cb8a5a4be8870f26f32b4b9
2222-42 <stephen0617@me.com>
2021-07-25 07:51:54 +0900

    chore(chap9): basics

---
+++ chapter09/sampleFile/sampleOpen.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func create() {
+	file, err := os.Create("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "New file content\n")
+}
+
+func open() {
+	file, err := os.Open("textfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fmt.Println("Read file:")
+	io.Copy(os.Stdout, file)
+}
+
+func append() {
+	file, err := os.OpenFile("textfile.txt", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	io.WriteString(file, "Appended content\n")
+}
+
+func main() {
+	create()
+	open()
+	append()
+	open()
+}
