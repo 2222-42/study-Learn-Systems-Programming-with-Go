003f2c0e3900c6c22dc20e94ae13c0d357079676
2222-42 <stephen0617@me.com>
2021-07-31 18:23:16 +0900

    chore(chap9): filepath.Split

--- chapter09/samplePath/sampleJoin/concatPath.go
+++ chapter09/samplePath/sampleJoin/concatPath.go
@@ -6,6 +6,16 @@ import (
 	"path/filepath"
 )
 
-func main() {
+func concat() {
 	fmt.Printf("Temp File Path : %s\n", filepath.Join(os.TempDir(), "temp.txt"))
+}
+
+func split() {
+	dir, name := filepath.Split(os.Getenv("GOPATH"))
+	fmt.Printf("Dir: %s, Name: %s\n", dir, name)
+}
+
+func main() {
+	concat()
+	split()
 }
