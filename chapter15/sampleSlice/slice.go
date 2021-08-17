3322fd69960aa8cce2b623d635487bb8e52246b5
2222-42 <stephen0617@me.com>
2021-08-17 09:49:24 +0900

    chore(chap15): use append
    新しく配列が作られることについての確認

--- chapter15/sampleSlice/slice.go
+++ chapter15/sampleSlice/slice.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func makeSlices() {
 	// 既存の配列を参照
 	a := [4]int{0, 1, 2, 3}
 	b := a[:]
@@ -23,4 +23,21 @@ func main() {
 
 	g := make([]int, 4, 8)
 	fmt.Println(&g[0], len(g), cap(g))
+}
+
+func makeSureOfMemory() {
+	s := make([]int, 1, 2)
+	fmt.Println(&s[0], len(s), cap(s))
+
+	s = append(s, 1)
+	fmt.Println(&s[0], len(s), cap(s))
+
+	// cap()の余裕がなくなり、新たな配列が確保され、アドレスも変わる。
+	s = append(s, 2)
+	fmt.Println(&s[0], len(s), cap(s))
+}
+
+func main() {
+	makeSlices()
+	makeSureOfMemory()
 }
